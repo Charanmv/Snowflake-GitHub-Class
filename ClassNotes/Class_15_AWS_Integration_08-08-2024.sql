3d870a949f3ba4be6e75af182f6a2eaf66cf5dcf
charanmv <charanmv99@gmail.com>
2024-08-12 08:08:14 +0530

    file modified

--- ClassNotes/Class_15_AWS_Integration_08-08-2024.sql
+++ ClassNotes/Class_15_AWS_Integration_08-08-2024.sql
@@ -67,31 +67,51 @@ DESC integration s3_int;
 class notes:
 ---------------------
 
+s3://snowclass-vitech-practice/csv/
+s3://snowclass-vitech-practice/json/
+
+arn:aws:iam::024848483653:role/snowclass-vitech-practice
+
+
+arn:aws:iam::211125613752:user/externalstages/cib0y60000
+XU91440_SFCRole=2_Er9P1WnjM4q8VBrZwEXRzXBUU1Q=
+
+
+
+// create storage integration object
+
 create or replace storage integration s3_int
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = S3
   ENABLED = TRUE 
-  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::024848483653:role/snow-vitech-11am-role'
-  STORAGE_ALLOWED_LOCATIONS = ('s3://vitech-snow11am-batch/csv/', 's3://vitech-snow11am-batch/csv/')
-   COMMENT = 'This is an s3 integration object ' 
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::024848483653:role/snowclass-vitech-practice'
+  STORAGE_ALLOWED_LOCATIONS = ('s3://snowclass-vitech-practice/csv/', 's3://snowclass-vitech-practice/json/')
+   COMMENT = 'This is an s3 integration object '
 
-
-
-   --------------------------
-create database MANAGE_DB;
-
-create schema file_formats;
-
-	DESC integration s3_int;
-
+   // See storage integration properties to fetch external_id so we can update it in S3
+DESC integration s3_int;
 
 CREATE OR REPLACE stage stage_aws_csv
-    URL = 's3://vitech-snow11am-batch/csv/'
+    URL = 's3://snowclass-vitech-practice/csv/'
     STORAGE_INTEGRATION = s3_int
 
-list @stage_aws_csv
-    
-CREATE OR REPLACE file format MANAGE_DB.file_formats.csv_fileformat
+    list @stage_aws_csv
+
+    CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.movie_titles (
+  show_id STRING,
+  type STRING,
+  title STRING,
+  director STRING,
+  cast STRING,
+  country STRING,
+  date_added STRING,
+  release_year STRING,
+  rating STRING,
+  duration STRING,
+  listed_in STRING,
+  description STRING )
+
+  CREATE OR REPLACE file format MANAGE_DB.file_formats.csv_fileformat
     type = csv
     field_delimiter = ','
     skip_header = 1
@@ -103,73 +123,6 @@ copy into OUR_FIRST_DB.PUBLIC.movie_titles
   from @stage_aws_csv 
 file_format = (FORMAT_NAME  = MANAGE_DB.file_formats.csv_fileformat)
 files =('netflix_titles.csv')
-
-select count(*)  from  OUR_FIRST_DB.PUBLIC.movie_titles;
-
-select *  from  OUR_FIRST_DB.PUBLIC.movie_titles;
-    
-    -------------------------
-create database OUR_FIRST_DB;
-
-// Create table first
-CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.movie_titles (
-  show_id STRING,
-  type STRING,
-  title STRING,
-  director STRING,
-  cast STRING,
-  country STRING,
-  date_added STRING,
-  release_year STRING,
-  rating STRING,
-  duration STRING,
-  listed_in STRING,
-  description STRING )
-
-  create or replace storage integration s3_int
-  TYPE = EXTERNAL_STAGE
-  STORAGE_PROVIDER = S3
-  ENABLED = TRUE 
-  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::024848483653:role/snow-vitect-10am-role'
-  STORAGE_ALLOWED_LOCATIONS = ('s3://vitech-snow10am-batch/csv/')
-   COMMENT = 'This is an s3 integration object' 
-
-   DESC integration s3_int;
-
-   CREATE OR REPLACE stage stage_aws_csv
-    URL = 's3://vitech-snow10am-batch/csv/'
-    STORAGE_INTEGRATION = s3_int
-
-list @stage_aws_csv
-
-// Create table first
-CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.movie_titles (
-  show_id STRING,
-  type STRING,
-  title STRING,
-  director STRING,
-  cast STRING,
-  country STRING,
-  date_added STRING,
-  release_year STRING,
-  rating STRING,
-  duration STRING,
-  listed_in STRING,
-  description STRING )
-
-  CREATE OR REPLACE file format OUR_FIRST_DB.PUBLIC.csv_fileformat
-    type = csv
-    field_delimiter = ','
-    skip_header = 1
-    null_if = ('NULL','null')
-    empty_field_as_null = TRUE    
-    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
-
-    copy into OUR_FIRST_DB.PUBLIC.movie_titles  
-  from @stage_aws_csv 
-file_format = (FORMAT_NAME  = OUR_FIRST_DB.PUBLIC.csv_fileformat)
-files =('netflix_titles.csv')
-
 
 select count(*)  from  OUR_FIRST_DB.PUBLIC.movie_titles;
 
